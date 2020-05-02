7e0d3d5b9bad8e2fcf483684a1b947713f2975c1
ultimate-collaborator-4688 <aalbrecht33733@gmail.com>
2020-05-02 17:53:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+var dwarves = ["Dopey", "Grumpy", "Bashful"];
+
 function dwarfRollCall(dwarves) {
-}
+  dwarves.forEach(function(){
+    return "'1.' + dwarves[0] + '2.' + dwarves[1] "
+  });
+};
+
+
+
+
+
 
 function summonCaptainPlanet(planeteerCalls){
 }
